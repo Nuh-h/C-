bb3412038f3cde438b3ebf08e889ddcc16f21f72
Nuh <69435206+Nuh-h@users.noreply.github.com>
2021-01-09 14:29:14 +0000

    Razor pages project
    Uploading minimal number of files, focusing on the main ones. Ignoring important files such as bootstrap, jquery, site.js, site.css, assembly info etc.

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        //minute 1:05:24 
+
+        public string ISBN { get; set; }
+
+    }
+}
